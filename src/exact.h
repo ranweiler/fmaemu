e51111b3c2bcd34bec34f3bf7d09aeb7ffea0638
Joe Ranweiler <joe@lemma.co>
2018-01-02 11:35:59 -0800

    Use consistent header style

--- src/exact.h
+++ src/exact.h
@@ -4,7 +4,5 @@
 
 
 void split(double *x_out, double *y_out, double a);
-
 void exact_add(double *sum_appx, double *sum_err, double a, double b);
-
 void exact_mul(double *mul_appx, double *mul_err, double a, double b);
